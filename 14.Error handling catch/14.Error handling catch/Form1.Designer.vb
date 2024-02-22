a777ab80d214a2b7452f0302fb45c8296903354f
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-23 01:47:54 +0200

    Add files via upload

---
+++ 14.Error handling catch/14.Error handling catch/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        TextBox3 = New TextBox()
+        Label3 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(42, 41)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(36, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Index"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(42, 59)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(120, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(42, 148)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(246, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(42, 130)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(31, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Item"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(42, 255)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(246, 136)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(42, 237)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(32, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Error"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(188, 59)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(100, 23)
+        Button1.TabIndex = 6
+        Button1.Text = "Show Item"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(418, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ 14.Error handling catch/14.Error handling catch/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Dim MyList As New List(Of String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MyList.Add("Cat")
+        MyList.Add("Dog")
+        MyList.Add("horse")
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            TextBox2.Text = MyList(TextBox1.Text)
+        Catch ex As Exception
+            TextBox3.Text = ex.Message
+        End Try
+    End Sub
+End Class
+
